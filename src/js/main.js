295067c6ec1ff3dd4fd6b9599fd746b6f299e272
egorystuff <mr.yankovec@gmail.com>
2023-05-27 23:36:56 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -20,10 +20,10 @@ let tasks = [];
 
 // проверяем есть ли local Storage для записи в массив
 if (localStorage.getItem('tasks')) {
-  tasks = JSON.parse(localStorage.getItem('tasks'));
+	tasks = JSON.parse(localStorage.getItem('tasks'));
 
-  // передаем данные из localStorage в массив
-  tasks.forEach((task) => renderTask(task));
+	// передаем данные из localStorage в массив
+	tasks.forEach((task) => renderTask(task));
 }
 
 checkEmptyList();
@@ -34,108 +34,108 @@ checkBox();
 
 // функции
 function addTask(event) {
-  event.preventDefault();
+	event.preventDefault();
 
-  // Получите текст задачи из поля ввода
-  const taskText = taskInput.value;
+	// Получите текст задачи из поля ввода
+	const taskText = taskInput.value;
 
-  // создаем объект с задачами
-  const newTask = {
-    id: Date.now(),
-    text: taskText,
-    done: false,
-  };
+	// создаем объект с задачами
+	const newTask = {
+		id: Date.now(),
+		text: taskText,
+		done: false,
+	};
 
-  // добавляем задачу в массив
-  tasks.push(newTask);
+	// добавляем задачу в массив
+	tasks.push(newTask);
 
-  renderTask(newTask);
+	renderTask(newTask);
 
-  // clear the input field and return focus to it
-  taskInput.value = '';
-  taskInput.focus();
+	// clear the input field and return focus to it
+	taskInput.value = '';
+	taskInput.focus();
 
-  checkEmptyList();
+	checkEmptyList();
 
-  saveToLocalStorage();
+	saveToLocalStorage();
 
-  checkBox();
+	checkBox();
 
-  addDisabledBtn();
+	addDisabledBtn();
 }
 
-function deleteTask({target}) {
-  if (target.dataset.action !== 'delete') return;
+function deleteTask({ target }) {
+	if (target.dataset.action !== 'delete') return;
 
-  const parentNode = target.closest('li');
+	const parentNode = target.closest('li');
 
-  // определяем id задачи
-  const id = Number(parentNode.id);
+	// определяем id задачи
+	const id = Number(parentNode.id);
 
-  // находим индекс задачи в массиве
-  const index = tasks.findIndex((task) => task.id === id);
+	// находим индекс задачи в массиве
+	const index = tasks.findIndex((task) => task.id === id);
 
-  // удаляем задачу из массива
-  tasks.splice(index, 1);
+	// удаляем задачу из массива
+	tasks.splice(index, 1);
 
-  // 2 метод - удаляем задачу через фильтрацию массива
-  // tasks = tasks.filter((task) => task.id !== id);
+	// 2 метод - удаляем задачу через фильтрацию массива
+	// tasks = tasks.filter((task) => task.id !== id);
 
-  // удаляем задачу из разметки
-  parentNode.remove();
+	// удаляем задачу из разметки
+	parentNode.remove();
 
-  checkEmptyList();
+	checkEmptyList();
 
-  saveToLocalStorage();
+	saveToLocalStorage();
 }
 
-function doneTask({target}) {
-  if (target.dataset.action !== 'done') return;
+function doneTask({ target }) {
+	if (target.dataset.action !== 'done') return;
 
-  const parentNode = target.closest('li');
+	const parentNode = target.closest('li');
 
-  // определяем id задачи
-  const id = Number(parentNode.id);
+	// определяем id задачи
+	const id = Number(parentNode.id);
 
-  const task = tasks.find((task) => task.id === id);
+	const task = tasks.find((task) => task.id === id);
 
-  task.done = !task.done;
+	task.done = !task.done;
 
-  const taskTitle = parentNode.querySelector('span');
-  taskTitle.classList.toggle('task-title--done');
+	const taskTitle = parentNode.querySelector('span');
+	taskTitle.classList.toggle('task-title--done');
 
-  saveToLocalStorage();
+	saveToLocalStorage();
 }
 
 function checkEmptyList() {
-  // Проверка не вустой ли массив
-  if (tasks.length === 0) {
-    const emptyListHTML = `
+	// Проверка не вустой ли массив
+	if (tasks.length === 0) {
+		const emptyListHTML = `
     <li id="emptyList" class="list-group-item empty-list">
     <img src="./img/leaf.svg" alt="Empty" width="48" class="mt-3" />
     <div class="empty-list__title">Список дел пуст</div>
   </li>`;
 
-    tasksList.insertAdjacentHTML('afterbegin', emptyListHTML);
-  }
+		tasksList.insertAdjacentHTML('afterbegin', emptyListHTML);
+	}
 
-  if (tasks.length > 0) {
-    const emptyListEl = document.querySelector('#emptyList');
-    emptyListEl ? emptyListEl.remove() : null;
-  }
+	if (tasks.length > 0) {
+		const emptyListEl = document.querySelector('#emptyList');
+		emptyListEl ? emptyListEl.remove() : null;
+	}
 }
 
 function saveToLocalStorage() {
-  // сохранение массива в Local Storage браузера
-  localStorage.setItem('tasks', JSON.stringify(tasks));
+	// сохранение массива в Local Storage браузера
+	localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 function renderTask(task) {
-  // формируем CSS class
-  const cssClass = task.done ? 'task-title task-title--done' : 'task-title';
+	// формируем CSS class
+	const cssClass = task.done ? 'task-title task-title--done' : 'task-title';
 
-  // генерировать разметку для новой задачи
-  const taskHTML = `		
+	// генерировать разметку для новой задачи
+	const taskHTML = `		
   <li id="${task.id}"  class="list-group-item d-flex justify-content-between task-item">
   <span class="${cssClass}">${task.text}</span>
   <div class="task-item__buttons">
@@ -148,107 +148,122 @@ function renderTask(task) {
   </div>
 </li>`;
 
-  // add a task to the page
-  tasksList.insertAdjacentHTML('beforeend', taskHTML);
+	// add a task to the page
+	tasksList.insertAdjacentHTML('beforeend', taskHTML);
 }
 
 function addClassHeart() {
-  if (heart.classList.contains('fa-heart--red')) heart.classList.remove('fa-heart--red');
-  else heart.classList.add('fa-heart--red');
+	if (heart.classList.contains('fa-heart--red')) heart.classList.remove('fa-heart--red');
+	else heart.classList.add('fa-heart--red');
 }
 
 function keyHandlerEsc(e) {
-  if (e.keyCode === 27 || e.key === 'Escape') {
-    e.target.value = '';
-    e.target.blur();
-    addDisabledBtn();
-    return;
-  }
+	if (e.keyCode === 27 || e.key === 'Escape') {
+		e.target.value = '';
+		e.target.blur();
+		addDisabledBtn();
+		return;
+	}
 
-  if (e.key === 'Enter') {
-    const taskText = taskInput.value;
+	if (e.key === 'Enter') {
+		const taskText = taskInput.value;
 
-    // создаем объект с задачами
-    const newTask = {
-      id: Date.now(),
-      text: taskText,
-      done: false,
-    };
+		// создаем объект с задачами
+		const newTask = {
+			id: Date.now(),
+			text: taskText,
+			done: false,
+		};
 
-    tasks.push(newTask);
+		tasks.push(newTask);
 
-    renderTask(newTask);
+		renderTask(newTask);
 
-    taskInput.value = '';
-    taskInput.blur();
+		taskInput.value = '';
+		taskInput.blur();
 
-    addDisabledBtn();
-    taskInput.focus();
+		addDisabledBtn();
+		taskInput.focus();
 
-    checkEmptyList();
+		checkEmptyList();
 
-    saveToLocalStorage();
+		saveToLocalStorage();
 
-    checkBox();
+		checkBox();
 
-    addDisabledBtn();
-
-
-  }
+		addDisabledBtn();
+	}
 }
 
+const cross = document.getElementById('cross');
 
-const cross = document.getElementById('cross')
-
-cross.addEventListener("click", () => {
-  taskInput.value = '';
-  cross.classList.remove('active')
-  addDisabledBtn();
-})
+cross.addEventListener('click', () => {
+	taskInput.value = '';
+	cross.classList.remove('active');
+	addDisabledBtn();
+});
 
 function addDisabledBtn() {
-  btnAdd.setAttribute('disabled', true);
+	btnAdd.setAttribute('disabled', true);
 
-  taskInput.oninput = function () {
-    if (!taskInput.value.trim()) {
-      cross.classList.remove('active')
-      btnAdd.setAttribute('disabled', true);
-    } else {
-      cross.classList.add('active')
-      btnAdd.removeAttribute('disabled');
-    }
-  };
+	taskInput.oninput = function () {
+		if (!taskInput.value.trim()) {
+			cross.classList.remove('active');
+			btnAdd.setAttribute('disabled', true);
+		} else {
+			cross.classList.add('active');
+			btnAdd.removeAttribute('disabled');
+		}
+	};
 }
 
 function checkBox() {
-  box.oninput = function () {
-    if (box.checked) {
-      btnAdd.setAttribute('disabled', true);
-      taskInput.setAttribute('disabled', true);
-      addDisabledBtnAll();
-    } else {
-      btnAdd.removeAttribute('disabled');
-      taskInput.removeAttribute('disabled');
-      dellDisabledBtnAll();
-      addDisabledBtn();
-    }
-  };
+	box.oninput = function () {
+		if (box.checked) {
+			btnAdd.setAttribute('disabled', true);
+			taskInput.setAttribute('disabled', true);
+			addDisabled();
+		} else {
+			btnAdd.removeAttribute('disabled');
+			taskInput.removeAttribute('disabled');
+			dellDisabled();
+			addDisabledBtn();
+		}
+	};
 }
 
-function addDisabledBtnAll() {
-  const btnAll = document.querySelectorAll('button');
+function addDisabled() {
+	const btnAll = document.querySelectorAll('button');
+	const linksAll = document.querySelectorAll('a');
+	const I = document.querySelectorAll('i');
 
-  for (let i = 0; i < btnAll.length; i++) {
-    // bShow[i].setAttribute('disabled', true);
-    btnAll[i].classList.add('disabled');
-  }
+	for (let i = 0; i < btnAll.length; i++) {
+		btnAll[i].classList.add('disabled');
+	}
+
+	for (let i = 0; i < linksAll.length; i++) {
+		linksAll[i].classList.add('disabled');
+	}
+
+	for (let i = 0; i < I.length; i++) {
+		I[i].classList.add('disabled');
+	}
 }
 
-function dellDisabledBtnAll() {
-  const btnAll = document.querySelectorAll('button');
+function dellDisabled() {
+	const btnAll = document.querySelectorAll('button');
+	const linksAll = document.querySelectorAll('a');
+	const I = document.querySelectorAll('i');
 
-  for (let i = 0; i < btnAll.length; i++) {
-    // bShow[i].removeAttribute('disabled');
-    btnAll[i].classList.remove('disabled');
-  }
+	for (let i = 0; i < btnAll.length; i++) {
+		btnAll[i].classList.remove('disabled');
+	}
+
+	for (let i = 0; i < linksAll.length; i++) {
+		linksAll[i].classList.remove('disabled');
+	}
+
+	for (let i = 0; i < I.length; i++) {
+		I[i].classList.remove('disabled');
+	}
 }
